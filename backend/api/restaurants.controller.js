59f0bb9ff85a31e19dfa21d496042eb497bc7c50
Ezequiel <martinez.ezequiel42@gmail.com>
2022-02-15 06:08:34 -0800

    added getrestaurantcuisines to restaurants.controller.js

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -44,4 +44,14 @@ export default class RestaurantsController {
             res.status(500).json({ error: e})
         }
     }
+
+    static async apiGetRestaurantCuisines(req, res, next) {
+        try {
+            let cuisines = await RestaurantsDAO.getCuisines()
+            res.json(cuisines)
+        } catch(e) {
+            console.log(`api, ${e}`)
+            res.sataus(500).json({ error: e })
+        }
+    }
 }
