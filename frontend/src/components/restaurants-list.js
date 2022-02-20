f526a52d1591504e3107e6a9da2cb23f43a340c3
Ezequiel <martinez.ezequiel42@gmail.com>
2022-02-20 07:13:20 -0800

    created the restaurants, searchName, searchZip, searchCuisine, and cuisines states with their respective setstate functions

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -2,6 +2,47 @@ import React, {useState, useEffect } from "react";
 import RestaurantDataService from "../services/restaurant";
 import { Link } from "react-router-dom";
 
+
+const RestaurantsList = props => {
+    const [restaurants, setRestaurants] = useState([]);
+    const [searchName, setSearchName] = useState("");
+    const [searchZip, setSearchZip] = useState("");
+    const [searchCuisine, setSearchCuisine] = useState("");
+    const [cuisines, setCuisines] = useState(["All Cuisines"]);
+
+    useEffect(() => {
+        retrieveRestaurants();
+        retreiveCuisines();
+    }, []);
+
+    const onChangeSearchName = e => {
+        const searchName = e.target.value;
+        setSearchName(searchName);
+    };
+
+    const onChangeSearchZip = e => {
+        const searchZip = e.target.value;
+        setSearchZip(searchZip);
+    };
+
+    const onChangeSearchCuisine = e => {
+        const searchCuisine = e.target.value;
+        setSearchCuisine(searchCuisine);
+    };
+
+    const retrieveRestaurants = () => {
+        RestaurantDataService.getAll()
+        .then(response => {
+            console.log(response.data);
+            setRestaurants(response.data.restaurants);
+            
+        })
+        .catch(e => {
+            console.log(e);
+        });
+    };
+}
+
 function RestaurantsList() {
     return (
         <div className="App">
