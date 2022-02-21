d5b356e33e5455f5e88792bf4c35b4cc7a849e02
Ezequiel <martinez.ezequiel42@gmail.com>
2022-02-21 14:36:50 -0800

    completed restaurants-list.js component. Added final card rendering for all search resaults

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/resaurants",
+    baseURL: "http://localhost:5000/api/v1/restaurants",
     headers: {
         "Content-Type": "application/json"
     }
