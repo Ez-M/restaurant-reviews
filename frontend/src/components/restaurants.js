54d4a817309a4272d100245bd146fd0f786b94ef
Ezequiel <martinez.ezequiel42@gmail.com>
2022-02-26 00:08:38 -0800

    added getRestaurant, useEffect, and delteReview to restaurants.js component

--- frontend/src/components/restaurants.js
+++ frontend/src/components/restaurants.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+import RestaurantDataService from "../services/restaurant.js"
+import { Link } from "react-router-dom";
 
-function Restaurant() {
-    return (
-        <div className="App">
-            Hello World. restaurants
-        </div>
-    );
+
+const Restaurant = props => {
+    const initialRestaurantState = {
+        id: null,
+        name: "",
+        address: {},
+        cuisine: "",
+        reviews: []
+    }; 
+    const [restaurant, setRestaurant] = useState(initialRestaurantState);
+
+    const getRestaurant = id => {
+        RestaurantDataService.get(id)
+        .then(response => {
+            setRestaurant(response.data);
+            console.log(response.data);
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    };
+
+    useEffect(() => {
+        getRestaurant(props.match.params.id);
+    }, [props.match.params.id]);
+
+    const deleteReview =(reviewId, index) => {
+        RestaurantDataService.deleteReview(reviewId)
+        .then(response => {
+            setRestaurant((prevState) => {
+                prevState.reviews.splice(index,1)
+                return({
+                    ...prevState
+                })
+            })
+        })
+        .catch(e => {
+            console.log(e);
+        });
+    }
 }
 
 export default Restaurant;
